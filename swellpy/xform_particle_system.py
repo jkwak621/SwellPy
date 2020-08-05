a7229209c27188b78c2033ac27f97f27077b3f45
Jenny Kwak <65518279+jkwak621@users.noreply.github.com>
2020-08-05 14:46:04 -0700

    Create xform_particle_system.py

---
+++ swellpy/xform_particle_system.py
@@ -0,0 +1,157 @@
+'''
+from jakemandel fork
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+from scipy.spatial import cKDTree
+from peakutils import peak
+import time
+import pickle
+import crepel
+
+""" 
+This class defines features that are applicable to both Monodisperse
+and Bidisperse systems. It is not intended to be insantiated, but 
+can be. It is meant as a building block for other types of particle
+systems. Since this class consideres the size of particles to be 
+undefined, this system cannot be trained, tagged or repelled. It
+can set centers and wrap particles that are outside of the box. 
+These operations are all independent of particle
+size/shape. 
+Note there are some ``private'' methods that are implemented in this
+class that are used by child classes, but not by this class. 
+Calling these methods from the ParticleSuspension class will not be
+successful. 
+``Save'' and ``Load'' functionality is also defined in this file
+and is NOT a method of the ParticleSystem class.
+"""
+
+class ParticleSystem2():
+    def __init__(self, N, boxsize_x=None, boxsize_y=None, seed=None):
+        self._name = "ParticleSystem2"
+        self.N = N
+        if (boxsize_x) and (boxsize_y):
+            self.boxsize = np.sqrt(boxsize_x*boxsize_y)
+            self.boxsize_x = boxsize_x
+            self.boxsize_y = boxsize_y
+        else:
+            self.boxsize = np.sqrt(N*np.pi/(4 * 0.2))
+        self.centers = None
+        self.reset(seed)
+    
+
+    def reset(self, seed=None):
+        """ Randomly positions the particles inside the box.
+        
+        Args:
+            seed (int): optional. The seed to use for randomization 
+        """
+        if (isinstance(seed, int)):
+            np.random.seed(seed)
+        self.centers = np.random.uniform(0, self.boxsize, (self.N, 2))
+    
+    def wrap(self):
+        """
+        Applies periodic boundaries to any particles outside of the box. 
+        """
+        centers = self.centers
+        boxsize = self.boxsize
+        # Wrap if outside of boundaries
+        np.putmask(centers, centers>=boxsize, centers % boxsize)
+        np.putmask(centers, centers<0, centers % boxsize)
+
+    
+    def pos_noise(self, sigma):
+        """
+        adds random noise to the position of each particle, typically used before each swell
+        
+        Args: 
+            sigma: standard deviation for the gaussian used to generate the kick
+        """
+        
+        centers = self.centers
+        boxsize = self.boxsize
+        kicks = np.random.normal(0, sigma, size=np.shape(centers))
+        self.centers = centers+kicks
+        
+    
+    def _repel(self, pairs, swell, kick):
+        """ 
+        Repels particles that overlap
+        
+        Args:
+            pairs ((M x 2) np.array): An array object whose elements are pairs of int values that 
+                correspond to the center indices of overlapping particles
+            swell (float): Swollen diameter length of the particles
+            kick (float): The maximum distance particles are repelled 
+        """
+        if not isinstance(pairs, np.ndarray):
+            pairs = np.array(pairs, dtype=np.int64)
+        pairs = pairs.astype(np.int64) # This is necessary for the c extension
+        centers = self.centers
+        boxsize = self.boxsize
+        # Fill the tagged pairs with the center coordinates
+        fillTagged = np.take(centers, pairs, axis=0)
+        # Find the position of the second particle in the tagged pair
+        # with respect to the first 
+        separation = np.diff(fillTagged, axis=1)
+        # Account for tagged across periodic bounary
+        np.putmask(separation, separation > swell, separation - boxsize)
+        np.putmask(separation, separation < -swell, separation + boxsize)
+        # Normalize
+        norm = np.linalg.norm(separation, axis=2).flatten()
+        unitSeparation = (separation.T/norm).T
+        # Generate kick
+        kick_arr = (unitSeparation.T * np.random.uniform(0, kick, unitSeparation.shape[0])).T
+        # Since the separation is with respect to the 'first' particle in a pair, 
+        # apply positive kick to the 'second' particle and negative kick to the first
+        crepel.iterate(centers, pairs[:,1], kick_arr, pairs.shape[0])
+        crepel.iterate(centers, pairs[:,0], -kick_arr, pairs.shape[0])
+        # Note: this may kick out of bounds -- be sure to wrap!
+    
+    def set_centers(self, centers):
+        """ Manually set the position of particles in the box. Raises an exception if 
+        centers are not of proper format. Raises a warning if the particles
+        are placed outside of the box
+        
+        Args:
+            centers ((N x 2) array-like): An array or array-like object containing the x, y coordinates 
+                of each particle 
+        """
+        if not isinstance(centers, np.ndarray):
+            centers = np.array(centers, dtype=np.float64)
+        if (centers.shape) != (self.N, 2):
+            raise TypeError("Error: Centers must be a (%s, 2) array-like object" %self.N)
+        if ( (centers < 0).any() or (centers > self.boxsize).any() ):
+            raise RuntimeError("Warning: centers out of bounds (0, %0.2f)" %self.boxsize)
+        self.centers = centers
+
+def save(system, filename = None):
+    """
+    Pickles the current particle suspension. Filename is generated
+    from the day, time, number of particles, and type of system.
+    Args:
+        filename (string): optional. The filename used to save the system.
+    """
+    if (filename):
+        f = open(filename, "wb")
+    else:
+        f = open(system.name + "_%d_%s_%s.p" 
+            %(system.N, time.strftime("%d-%m-%Y"), time.strftime("%H.%M.%S")), "wb")
+    pickle.dump(system, f)
+    f.close()
+
+def load(filename):
+    """
+    Loads a pickled file from the current directory
+    
+    Args:
+        filename (string): The name of the pickled particle file
+    Return:
+        (ParticleSystem): The particle system
+    """
+    f = open(filename, "rb")
+    x = pickle.load(f)
+    f.close()
+    return x
